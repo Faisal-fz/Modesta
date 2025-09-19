da1e1771215da5c58b9fe996498ef77f2e10bca5
Faisal Faiz <faisalfaizsiddiqui@gmail.com>
2025-09-19 23:24:52 +0530

    feat: add user authentication schemas and database tables
    - Implemented sign-in and sign-up form validation schemas using Zod.
    - Added new User, Account, Session, and VerificationToken tables to the database schema.
    - Updated Prisma schema to reflect new user-related models and relationships.
    - Added necessary foreign key constraints and unique indexes for user email.
    - Updated package dependencies for authentication and database handling.

--- components/shared/header/menu.tsx
+++ components/shared/header/menu.tsx
@@ -8,7 +8,7 @@ import {
 } from "@/components/ui/sheet";
 import Link from "next/link";
 import ModeToggle from "./mode-toggle";
-
+import UserButton from "./user-button";
 const Menu = () => {
   return (
     <>
@@ -21,12 +21,7 @@ const Menu = () => {
               Cart
             </Link>
           </Button>
-          <Button asChild>
-            <Link href="/sign-in">
-              <UserIcon />
-              Sign In
-            </Link>
-          </Button>
+          <UserButton />
         </nav>
         <nav className="md:hidden">
           <Sheet>
@@ -42,12 +37,7 @@ const Menu = () => {
                   Cart
                 </Link>
               </Button>
-              <Button asChild>
-                <Link href="/sign-in">
-                  <UserIcon />
-                  Sign In
-                </Link>
-              </Button>
+              <UserButton />
             </SheetContent>
           </Sheet>
         </nav>
