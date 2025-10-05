73f1e67c855e3eadb9a7ad33abb2ffbe0e14cc17
Faisal Faiz <faisalfaizsiddiqui@gmail.com>
2025-10-05 16:29:14 +0530

    feat: integrate Stripe payment processing and success handling in order details

--- app/(root)/order/[id]/order-details-table.tsx
+++ app/(root)/order/[id]/order-details-table.tsx
@@ -27,14 +27,17 @@ import {
 import { toast } from "sonner";
 import { useTransition } from "react";
 import { Button } from "@/components/ui/button";
+import StripePayment from "./stripe-payment";
 const OrderDetailsTable = ({
   order,
   paypalClientId,
   isAdmin,
+  stripeClientSecret,
 }: {
   order: Order;
   paypalClientId: string;
   isAdmin: boolean;
+  stripeClientSecret: string | null;
 }) => {
   const {
     id,
@@ -216,6 +219,13 @@ const OrderDetailsTable = ({
                   />
                 </PayPalScriptProvider>
               </div>
+            )}
+            {!isPaid && paymentMethod === "Stripe" && stripeClientSecret && (
+              <StripePayment
+                priceInCents={Number(order.totalPrice) * 100}
+                orderId={order.id}
+                clientSecret={stripeClientSecret}
+              />
             )}
             {isAdmin && !isPaid && paymentMethod === "CashOnDelivery" && (
               <MarkAsPaidButton />
