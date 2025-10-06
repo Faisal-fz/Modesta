3b50aaddecb831fc02290d212cbb2198ff8b847b
Faisal Faiz <faisalfaizsiddiqui@gmail.com>
2025-10-06 15:00:42 +0530

    feat: implemented icon-boxes and deal-countdown

--- lib/constants/index.ts
+++ lib/constants/index.ts
@@ -9,8 +9,8 @@ export const SERVER_URL =
 export const LATEST_PRODUCTS_LIMIT =
   Number(process.env.LATEST_PRODUCTS_LIMIT) || 4;
 export const signInDefaultValues = {
-  email: "admin@example.com",
-  password: "123456",
+  email: "",
+  password: "",
 };
 export const signUpDefaultValues = {
   name: "Steve Smith",
